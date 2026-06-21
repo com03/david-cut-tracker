db486537977346d7496734a1d7365f12b8e25946
com03 <davidfernandez0215@gmail.com>
2026-06-21 00:24:24 -0400

    Disable stale GitHub Pages service worker

--- sw.js
+++ sw.js
@@ -1,45 +1,21 @@
-const CACHE_NAME = 'david-cut-tracker-direct-v1';
-const APP_ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './app-icon.svg'
-];
+const CACHE_NAME = 'david-cut-tracker-kill-switch-v1';
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_ASSETS))
-  );
-  self.skipWaiting();
+  event.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => Promise.all(
-      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-    ))
-  );
-  self.clients.claim();
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map((key) => caches.delete(key)));
+    await self.registration.unregister();
+    const clients = await self.clients.matchAll({ type: 'window', includeUncontrolled: true });
+    for (const client of clients) {
+      client.navigate(client.url);
+    }
+  })());
 });
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return;
-
-  const url = new URL(event.request.url);
-  if (url.origin !== self.location.origin) return;
-
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request).catch(() => caches.match('./index.html', { ignoreSearch: true }))
-    );
-    return;
-  }
-
-  event.respondWith(
-    fetch(event.request).then((response) => {
-      const copy = response.clone();
-      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-      return response;
-    }).catch(() => caches.match(event.request, { ignoreSearch: true }))
-  );
+self.addEventListener('fetch', () => {
+  return;
 });
